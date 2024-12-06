b82617ed2296c97a5050ba095b70e1a4acc5b6f1
chillpert <dev@christianhilpert.com>
2024-12-06 13:59:09 +0100

    Initial commit

--- init.lua
+++ init.lua
@@ -1,89 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-========                                    .-----.          ========
-========         .----------------------.   | === |          ========
-========         |.-""""""""""""""""""-.|   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||   KICKSTART.NVIM   ||   |-----|          ========
-========         ||                    ||   | === |          ========
-========         ||                    ||   |-----|          ========
-========         ||:Tutor              ||   |:::::|          ========
-========         |'-..................-'|   |____o|          ========
-========         `"")----------------(""`   ___________      ========
-========        /::::::::::|  |::::::::::\  \ no mouse \     ========
-========       /:::========|  |==hjkl==:::\  \ required \    ========
-========      '""""""""""""'  '""""""""""""'  '""""""""""'   ========
-========                                                     ========
-=====================================================================
-=====================================================================
-
-What is Kickstart?
-
-  Kickstart.nvim is *not* a distribution.
-
-  Kickstart.nvim is a starting point for your own configuration.
-    The goal is that you can read every line of code, top-to-bottom, understand
-    what your configuration is doing, and modify it to suit your needs.
-
-    Once you've done that, you can start exploring, configuring and tinkering to
-    make Neovim your own! That might mean leaving Kickstart just the way it is for a while
-    or immediately breaking it into modular pieces. It's up to you!
-
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
-
-Kickstart Guide:
-
-  TODO: The very first thing you should do is to run the command `:Tutor` in Neovim.
-
-    If you don't know what this means, type the following:
-      - <escape key>
-      - :
-      - Tutor
-      - <enter key>
-
-    (If you already know the Neovim basics, you can skip this step.)
-
-  Once you've completed that, you can continue working through **AND READING** the rest
-  of the kickstart init.lua.
-
-  Next, run AND READ `:help`.
-    This will open up a help window with some basic information
-    about reading, navigating and searching the builtin help documentation.
-
-    This should be the first place you go to look when you're stuck or confused
-    with something. It's one of my favorite Neovim features.
-
-    MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
-    which is very useful when you're not exactly sure of what you're looking for.
-
-  I have left several `:help X` comments throughout the init.lua
-    These are hints about where to find more information about the relevant settings,
-    plugins or Neovim features used in Kickstart.
-
-   NOTE: Look for lines like this
-
-    Throughout the file. These are for you, the reader, to help you understand what is happening.
-    Feel free to delete them once you know what you're doing, but they should serve as a guide
-    for when you are first encountering a few different constructs in your Neovim config.
-
-If you experience any errors while trying to install kickstart, run `:checkhealth` for more info.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now! :)
---]]
-
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
@@ -91,7 +5,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +16,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -146,7 +60,10 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Hide tab symbol to reduce noise
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
@@ -156,6 +73,12 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
+
+-- Hide tilde symbols that indicate empty lines (very annoying with no-neck-pain)
+vim.opt.fillchars = { eob = ' ' }
+
+-- Set tab stop
+vim.opt.tabstop = 4
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -174,12 +97,6 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-
--- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -615,7 +532,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
@@ -627,6 +544,8 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
+        -- This is a list of all available language servers
+        -- https://github.com/williamboman/mason-lspconfig.nvim?tab=readme-ov-file#available-lsp-servers
 
         lua_ls = {
           -- cmd = {...},
@@ -642,6 +561,11 @@ require('lazy').setup({
             },
           },
         },
+
+        bashls = {},
+        cmake = {},
+        pylsp = {},
+        cpplint = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -695,7 +619,8 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        -- local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
@@ -745,6 +670,9 @@ require('lazy').setup({
           --   end,
           -- },
         },
+        {
+          'onsails/lspkind.nvim',
+        },
       },
       'saadparwaiz1/cmp_luasnip',
 
@@ -785,7 +713,9 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<Return>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
@@ -828,7 +758,18 @@ require('lazy').setup({
           },
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
+          { name = 'codeium' },
           { name = 'path' },
+        },
+        formatting = {
+          fields = { 'abbr', 'kind', 'menu' },
+          expandable_indicator = true,
+          format = require('lspkind').cmp_format {
+            mode = 'symbol',
+            maxwidth = 50,
+            ellipsis_char = '...',
+            symbol_map = { Codeium = '🤖' },
+          },
         },
       }
     end,
@@ -839,16 +780,18 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'Mofiqul/vscode.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('vscode').setup {
+        transparent = true,
+      }
+    end,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+      vim.cmd.colorscheme 'vscode'
     end,
   },
 
@@ -898,7 +841,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'cpp', 'python', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -917,6 +860,110 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  {
+    'ibhagwan/fzf-lua',
+    -- optional for icon support
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      -- calling `setup` is optional for customization
+      require('fzf-lua').setup {
+        vim.keymap.set('n', '<Leader>b', "<cmd>lua require('fzf-lua').buffers()<CR>"),
+        vim.keymap.set('n', '<C-p>', "<cmd>lua require('fzf-lua').files()<CR>"),
+        vim.keymap.set('n', '<Leader>o', "<cmd>lua require('fzf-lua').git_files()<CR>"),
+        vim.keymap.set('n', '<Leader>hf', "<cmd>lua require('fzf-lua').oldfiles()<CR>"),
+        vim.keymap.set('n', '<Leader>a', "<cmd>lua require('fzf-lua').live_grep()<CR>"),
+        vim.keymap.set('n', '<Leader>t', "<cmd>lua require('fzf-lua').tags_grep_cword()<CR>"),
+        vim.keymap.set('n', 'gr', "<cmd>lua require('fzf-lua').lsp_references()<CR>"),
+        vim.keymap.set('n', 'gd', "<cmd>lua require('fzf-lua').lsp_definitions()<CR>"),
+        vim.keymap.set('n', 'gD', "<cmd>lua require('fzf-lua').lsp_declarations()<CR>"),
+        vim.keymap.set('n', 'gi', "<cmd>lua require('fzf-lua').lsp_implementations()<CR>"),
+        vim.keymap.set('n', '<Leader>s', "<cmd>lua require('fzf-lua').lsp_document_symbols()<CR>"),
+        vim.keymap.set('n', '<Leader>ca', "<cmd>lua require('fzf-lua').lsp_code_actions()<CR>"),
+        vim.keymap.set('n', '<Leader>co', "<cmd>lua require('fzf-lua').commands()<CR>"),
+        vim.keymap.set('n', '<Leader>hc', "<cmd>lua require('fzf-lua').command_history()<CR>"),
+        vim.keymap.set('n', '<Leader>hs', "<cmd>lua require('fzf-lua').search_history()<CR>"),
+        vim.keymap.set('n', '<Leader>m', "<cmd>lua require('fzf-lua').marks()<CR>"),
+        vim.keymap.set('n', '<Leader>km', "<cmd>lua require('fzf-lua').keymaps()<CR>"),
+        vim.keymap.set('n', '<Leader>rtfm', "<cmd>lua require('fzf-lua').man_pages()<CR>"),
+        vim.keymap.set('n', '<Leader>help', "<cmd>lua require('fzf-lua').help_tags()<CR>"),
+      }
+    end,
+  },
+  {
+    'shortcuts/no-neck-pain.nvim',
+    config = function()
+      require('no-neck-pain').setup {
+        width = 150,
+        autocmds = {
+          -- When `true`, enables the plugin when you start Neovim.
+          -- If the main window is  a side tree (e.g. NvimTree) or a dashboard, the command is delayed until it finds a valid window.
+          -- The command is cleaned once it has successfuly ran once.
+          --- @type boolean
+          enableOnVimEnter = true,
+          -- When `true`, enables the plugin when you enter a new Tab.
+          -- note: it does not trigger if you come back to an existing tab, to prevent unwanted interfer with user's decisions.
+          --- @type boolean
+          enableOnTabEnter = true,
+          -- When `true`, reloads the plugin configuration after a colorscheme change.
+          --- @type boolean
+          reloadOnColorSchemeChange = false,
+          -- When `true`, entering one of no-neck-pain side buffer will automatically skip it and go to the next available buffer.
+          --- @type boolean
+          skipEnteringNoNeckPainBuffer = false,
+        },
+      }
+    end,
+  },
+  {
+    'Exafunction/codeium.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      require('codeium').setup {
+        -- This makes codeium auto completion show up inline
+        virtual_text = {
+          enabled = true,
+
+          -- These are the defaults
+
+          -- Set to true if you never want completions to be shown automatically.
+          manual = false,
+          -- A mapping of filetype to true or false, to enable virtual text.
+          filetypes = {},
+          -- Whether to enable virtual text of not for filetypes not specifically listed above.
+          default_filetype_enabled = true,
+          -- How long to wait (in ms) before requesting completions after typing stops.
+          idle_delay = 75,
+          -- Priority of the virtual text. This usually ensures that the completions appear on top of
+          -- other plugins that also add virtual text, such as LSP inlay hints, but can be modified if
+          -- desired.
+          virtual_text_priority = 65535,
+          -- Set to false to disable all key bindings for managing completions.
+          map_keys = true,
+          -- The key to press when hitting the accept keybinding but no completion is showing.
+          -- Defaults to \t normally or <c-n> when a popup is showing.
+          accept_fallback = nil,
+          -- Key bindings for managing completions in virtual text mode.
+          key_bindings = {
+            -- Accept the current completion.
+            accept = '<Tab>',
+            -- Accept the next word.
+            accept_word = false,
+            -- Accept the next line.
+            accept_line = false,
+            -- Clear the virtual text.
+            clear = false,
+            -- Cycle to the next completion.
+            next = '<M-]>',
+            -- Cycle to the previous completion.
+            prev = '<M-[>',
+          },
+        },
+      }
+    end,
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -927,12 +974,12 @@ require('lazy').setup({
   --  Here are some example plugins that I've included in the Kickstart repository.
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
@@ -966,5 +1013,38 @@ require('lazy').setup({
   },
 })
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+-- Fix ! commands in Windows
+-- vim.o.shellcmdflag = '-c'
+-- vim.o.shell = '"C:/Program Files/Git/bin/bash.exe"'
+-- vim.o.shellredir = '>%s 2>&1'
+-- vim.o.shellpipe = '2>&1| tee'
+
+-- vim.o.shell = 'powershell'
+-- vim.o.shellcmdflag =
+--   '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+-- vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+-- vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+-- vim.o.shellquote = ''
+-- vim.o.shellxquote = ''
+
+-- @bug This causes codeium to break
+-- vim.o.shellcmdflag = '-c'
+-- vim.o.shellredir = '>%s 2>&1'
+-- vim.cmd [[
+-- if has("win32")
+--   let $TMP="/tmp"
+--   let &shell='"C:/Program Files/Git/bin/bash.exe"'
+--   let &shellcmdflag = '-c'
+--   let &shellredir = '>%s 2>&1'
+--   set shellquote= shellxescape=
+--   " set noshelltemp
+--   set shellxquote=
+--   let &shellpipe='2>&1| tee'
+-- endif
+-- ]]
+
+-- Quick cpp compile
+
+-- create a lua function that compiles the current file using g++ using cmd
+
+vim.keymap.set('n', '<S-F5>', ':!g++ % -o a.out && ./a.out<Return>', { desc = 'Compile the currently opened cpp file', noremap = true, silent = true })
